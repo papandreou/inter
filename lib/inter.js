papandreou/inter

commit e634f93fc49700980e533630409faf81e79b6ec2

Restructured the CLDR extraction code into a class.
Extract data for all calendars, not just the Gregorian one, but default to Gregorian in the renderDate etc. methods if no other calendar id is specified.

buildLocale: Added --daynames, --monthnames, --quarternames, and --eranames switches.

lib/inter.js
@@ -295,13 +295,13 @@ inter = {
      * @return {Function} The date renderer.
      * @private (use renderDate or getDateRenderer)
      */
-    makeDateRenderer: function (formatId) {
+    makeDateRenderer: function (formatId, calendarId) {
         var format;
         if (formatId === 'hv') {
             // Hack: There's no 'hv' format in CLDR. Try to make one by stripping the minute part from the short time format:
-            format = this.getDateFormat("shortTime").replace(/[\.:]i\s*/, "");
+            format = this.getDateFormat("shortTime", calendarId).replace(/[\.:]i\s*/, "");
         } else {
-            format = this.getDateFormat(formatId);
+            format = this.getDateFormat(formatId, calendarId);
             if (!format) {
                 throw new Error('inter.makeDateRenderer: Cannot find date format: ' + formatId);
             }
@@ -326,15 +326,16 @@ inter = {
      * (Object{start: Date, end: Date} => String).
      * @private (use renderDateInterval or getDateIntervalRenderer)
      */
-    makeDateIntervalRenderer: function (formatId, datePartOnly) {
-        var greatestDifferences = this.dateIntervalFormats[formatId];
+    makeDateIntervalRenderer: function (formatId, datePartOnly, calendarId) {
+        calendarId = calendarId || 'gregorian';
+        var greatestDifferences = this.calendars[calendarId].dateIntervalFormats[formatId];
         if (!greatestDifferences) {
-            var bestMatchingDateIntervalFormatId = this.getBestICUFormatId(formatId, this.dateIntervalFormats);
+            var bestMatchingDateIntervalFormatId = this.getBestICUFormatId(formatId, this.dateIntervalFormats, calendarId);
             if (bestMatchingDateIntervalFormatId) {
                 // Clone the best match, then adapt it:
                 greatestDifferences = {};
-                for (var key in this.dateIntervalFormats[bestMatchingDateIntervalFormatId]) {
-                    greatestDifferences[key] = this.adaptICUFormat(this.dateIntervalFormats[bestMatchingDateIntervalFormatId][key], formatId);
+                for (var key in this.calendars[calendarId].dateIntervalFormats[bestMatchingDateIntervalFormatId]) {
+                    greatestDifferences[key] = this.adaptICUFormat(this.calendars[calendarId].dateIntervalFormats[bestMatchingDateIntervalFormatId][key], formatId);
                 }
             }
         }
@@ -349,17 +350,17 @@ inter = {
                 var dateFormatId = matchFormatId[1],
                     timeFormatId = matchFormatId[2];
                 return function (dateInterval) {
-                    return inter.renderDateInterval(dateInterval, dateFormatId);
+                    return inter.renderDateInterval(dateInterval, dateFormatId, calendarId);
                 };
             } else {
                 // Create a fallback date interval renderer from the date format and the date interval fallback format:
-                var dateFormat = this.getDateFormat(formatId);
+                var dateFormat = this.getDateFormat(formatId, calendarId);
                 if (dateFormat) {
                     return this.getPatternRenderer(this.dateIntervalFallbackFormat,
                                                    this.makeDateRendererSource('values.start', dateFormat),
                                                    this.makeDateRendererSource('values.end', dateFormat));
                 } else {
-                    throw new Error("inter.renderDateInterval: No usable date interval format found for " + formatId);
+                    throw new Error("inter.renderDateInterval: No usable date interval format found for " + formatId + " (" + calendarId + ")");
                 }
             }
         }
@@ -384,31 +385,32 @@ inter = {
      * @return {String} The date format in ICU format, or undefined if no usable
      * format could be found.
      */
-    getDateFormat: function (formatId) {
-        var icuFormat = this.dateFormats.basic[formatId] || this.dateFormats.cldr[formatId];
+    getDateFormat: function (formatId, calendarId) {
+        calendarId = calendarId || 'gregorian';
+        var icuFormat = this.calendars[calendarId].dateFormats.basic[formatId] || this.calendars[calendarId].dateFormats.cldr[formatId];
         if (icuFormat) {
             return icuFormat;
         } else {
             // The exact format wasn't found.
             // See if we know a similar format that can be rewritten, explanation here: http://unicode.org/cldr/trac/ticket/2641
-            var bestCandidateFormatId = this.getBestICUFormatId(formatId, this.dateFormats.cldr);
+            var bestCandidateFormatId = this.getBestICUFormatId(formatId, this.dateFormats.cldr, calendarId);
             if (bestCandidateFormatId) {
-                return (this.dateFormats.cldr[formatId] = this.adaptICUFormat(this.dateFormats.cldr[bestCandidateFormatId], formatId));
+                return (this.calendars[calendarId].dateFormats.cldr[formatId] = this.adaptICUFormat(this.calendars[calendarId].dateFormats.cldr[bestCandidateFormatId], formatId));
             } else {
                 // No suitable formats found
                 var matchFormatId = formatId.match(/^y+M+d+$/);
                 if (matchFormatId) {
                     // For some reason there's no yMd fragment in CLDR, adapt the short date format to the required level of detail:
-                    return (this.dateFormats.cldr[formatId] = this.adaptICUFormat(this.dateFormats.basic.shortDate, formatId));
+                    return (this.calendars[calendarId].dateFormats.cldr[formatId] = this.adaptICUFormat(this.dateFormats.basic.shortDate, formatId));
                 }
 
                 matchFormatId = formatId.match(/^([yMQEd]+)([Hhms]+)$/);
                 if (matchFormatId) {
                     // It's a format with both date and time components. Try to lookup the date and time parts separately,
                     // then compose them using the default date time pattern:
-                    var dateFormat = this.getDateFormat(matchFormatId[1]),
-                        timeFormat = this.getDateFormat(matchFormatId[2]);
-                    return dateFormat && timeFormat && this.renderPattern([timeFormat, dateFormat], this.defaultDateTimePattern);
+                    var dateFormat = this.getDateFormat(matchFormatId[1], calendarId),
+                        timeFormat = this.getDateFormat(matchFormatId[2], calendarId);
+                    return dateFormat && timeFormat && this.renderPattern([timeFormat, dateFormat], this.calendars[calendarId].defaultDateTimePattern);
                 } else {
                     return; // No usable date format found
                 }

bin/buildLocale
@@ -11,7 +11,7 @@ var fs = require('fs'),
     _ = require('underscore'),
     leftPad = require('../lib/leftPad'),
     normalizeLocaleId = require('../lib/normalizeLocaleId'),
-    cldr = require('../lib/cldr'),
+    Cldr = require('../lib/Cldr'),
     resolveAndTrimLocaleData = require('../lib/resolveAndTrimLocaleData'),
     commandLineOptions = require('optimist')
         .usage('$0 [--cldrpath <pathToCldr>] [-o <outputFileName>] --locale localeId,localeId...')
@@ -29,6 +29,26 @@ var fs = require('fs'),
         })
         .options('dateintervalformats', {
             describe: 'Whether to include date and time interval formats (inter.dateIntervalFormats)',
+            type: 'boolean',
+            default: false
+        })
+        .options('daynames', {
+            describe: 'Whether to include day names (inter.calendars[calendarId].dayNames). Implied by --dateformats and --dateintervalformats',
+            type: 'boolean',
+            default: false
+        })
+        .options('monthnames', {
+            describe: 'Whether to include month names (inter.calendars[calendarId].monthNames). Implied by --dateformats and --dateintervalformats',
+            type: 'boolean',
+            default: false
+        })
+        .options('eranames', {
+            describe: 'Whether to include era names (inter.calendars[calendarId].eraNames). Implied by --dateformats and --dateintervalformats',
+            type: 'boolean',
+            default: false
+        })
+        .options('quarternames', {
+            describe: 'Whether to include quarter names (inter.calendars[calendarId].quarterNames). Implied by --dateformats and --dateintervalformats',
             type: 'boolean',
             default: false
         })
@@ -227,25 +247,13 @@ targetLocaleIds.forEach(function (targetLocaleId) {
     });
 });
 
-var cldrCommonMainXmlFileNameByNormalizedLocaleId = {};
-
-fs.readdirSync(Path.resolve(commandLineOptions.cldrpath, "common", "main")).forEach(function (fileName) {
-    var matchFileName = fileName.match(/^(.*)\.xml$/);
-    if (matchFileName) {
-        cldrCommonMainXmlFileNameByNormalizedLocaleId[normalizeLocaleId(matchFileName[1])] =
-            Path.resolve(commandLineOptions.cldrpath, "common", "main", fileName);
-    }
-});
+var cldr = new Cldr(commandLineOptions.cldrpath);
 
 seq(Object.keys(doExtractByLocaleId))
     .parEach(function (localeId) {
-        var cb = this,
-            cldrCommonMainXmlFileName = cldrCommonMainXmlFileNameByNormalizedLocaleId[normalizeLocaleId(localeId)];
-        if (!cldrCommonMainXmlFileName) {
-            throw new Error('Cannot find xml file for ' + localeId + ' in ' + commandLineOptions.cldrpath);
-        }
-        fs.readFile(cldrCommonMainXmlFileName, 'utf-8', passError(cb, function (xmlString) {
-            localeDataByLocaleId[localeId] = cldr.extractLocaleDataFromXmlString(xmlString);
+        var cb = this;
+        cldr.getLocaleData(localeId, passError(cb, function (localeData) {
+            localeDataByLocaleId[localeId] = localeData;
             cb();
         }));
     })
@@ -253,8 +261,7 @@ seq(Object.keys(doExtractByLocaleId))
         var cb = this;
 
         if (commandLineOptions.pluralrules) {
-            fs.readFile(Path.resolve(commandLineOptions.cldrpath, 'common', 'supplemental', 'plurals.xml'), 'utf-8', passError(cb, function (xmlString) {
-                var getQuantityMethodAstByLocaleId = cldr.generateGetQuantityMethodAstsFromXmlString(xmlString, Object.keys(doExtractByLocaleId));
+            cldr.generateGetQuantityMethodAstByLocaleId(Object.keys(doExtractByLocaleId), passError(cb, function (getQuantityMethodAstByLocaleId) {
                 Object.keys(doExtractByLocaleId).forEach(function (localeId) {
                     localeDataByLocaleId[localeId].getQuantity = getQuantityMethodAstByLocaleId[localeId];
                 });
@@ -302,17 +309,21 @@ seq(Object.keys(doExtractByLocaleId))
 
         var localeIdVarAst = uglifyJs.parser.parse(commandLineOptions.localeidvar)[1][0][1],
             statementAsts = [];
+
         targetLocaleIds.forEach(function (targetLocaleId) {
-            statementAsts.push(['if',
-                                ['binary', '===', localeIdVarAst, ['string', normalizeLocaleId(targetLocaleId)]],
-                                ['block',
-                                    Object.keys(localeDataByTargetLocaleId[targetLocaleId]).map(function (propertyName) {
-                                        return ['stat',
-                                            ['assign', true, ['dot', ['name', 'inter'], propertyName], uglifyAst.objToAst(localeDataByTargetLocaleId[targetLocaleId][propertyName])]
-                                        ];
-                                    })
-                                ]
-                               ]);
+            var propertyNames = Object.keys(localeDataByTargetLocaleId[targetLocaleId]);
+            if (propertyNames.length > 0) {
+                statementAsts.push(['if',
+                                    ['binary', '===', localeIdVarAst, ['string', normalizeLocaleId(targetLocaleId)]],
+                                    ['block',
+                                        propertyNames.map(function (propertyName) {
+                                            return ['stat',
+                                                ['assign', true, ['dot', ['name', 'inter'], propertyName], uglifyAst.objToAst(localeDataByTargetLocaleId[targetLocaleId][propertyName])]
+                                            ];
+                                        })
+                                    ]
+                                   ]);
+            }
         });
 
         var omitByMethodName = {};

lib/cldr.js
@@ -1,253 +1,0 @@
-var libxmljs = require('libxmljs'),
-    normalizeLocaleId = require('./normalizeLocaleId'),
-    cldrPluralRuleToJavaScriptAst = require('./cldrPluralRuleToJavaScriptAst'),
-    uglifyJs = require('uglify-js'),
-    cldr = module.exports = {};
-
-cldr.extractLocaleDataFromXmlString = function (xmlString, cb) {
-    var localeData = {};
-        document = libxmljs.parseXmlString(xmlString);
-
-    localeData.localeDisplayNames = {};
-    document.find('/ldml/localeDisplayNames/languages/language').forEach(function (node) {
-        var localeId = normalizeLocaleId(node.attr('type').value());
-        localeData.localeDisplayNames[localeId] = localeData.localeDisplayNames[localeId] || node.text();
-    });
-
-    localeData.numberSymbols = {};
-    document.find("/ldml/numbers/symbols/*").forEach(function (numberSymbolNode) {
-        if (numberSymbolNode.name() === 'alias') {
-            return;
-        }
-        var numberSystemId = numberSymbolNode.parent().attr('numberSystem').value(),
-            symbolId = numberSymbolNode.name();
-        localeData.numberSymbols[numberSystemId] = localeData.numberSymbols[numberSystemId] || {};
-        localeData.numberSymbols[numberSystemId][symbolId] = localeData.numberSymbols[numberSystemId][symbolId] || numberSymbolNode.text();
-    });
-
-    localeData.eraNames = {};
-    ['eraNames', 'eraAbbr'].forEach(function (eraType) {
-        var typeInOutput = {eraNames: 'wide', eraAbbr: 'abbreviated'}[eraType];
-        localeData.eraNames[typeInOutput] = [];
-        document.find("/ldml/dates/calendars/calendar[@type='gregorian']/eras/" + eraType + "/era").forEach(function (eraNode) {
-            var type = parseInt(eraNode.attr('type').value(), 10);
-            localeData.eraNames[typeInOutput][type] = localeData.eraNames[typeInOutput][type] || eraNode.text();
-        });
-    });
-
-    localeData.quarterNames = {};
-    document.find("/ldml/dates/calendars/calendar[@type='gregorian']/quarters/quarterContext/quarterWidth/quarter").forEach(function (quarterNode) {
-        // Avoid dashes in width and context (so that dot notation can be used in JavaScript)
-        var context = quarterNode.parent().parent().attr('type').value().replace(/-/g, ''),
-            width = quarterNode.parent().attr('type').value().replace(/-/g, ''),
-            quarterNo = parseInt(quarterNode.attr('type').value(), 10) - 1;
-        localeData.quarterNames[context] = localeData.quarterNames[context] || {};
-        localeData.quarterNames[context][width] = localeData.quarterNames[context][width] || [];
-        localeData.quarterNames[context][width][quarterNo] = localeData.quarterNames[context][width][quarterNo] || quarterNode.text();
-    });
-
-    localeData.monthNames = {};
-    document.find("/ldml/dates/calendars/calendar[@type='gregorian']/months/monthContext/monthWidth/month").forEach(function (monthNode) {
-        // Avoid dashes in width and context (so that dot notation can be used in JavaScript)
-        var context = monthNode.parent().parent().attr('type').value().replace(/-/g, ''),
-            width = monthNode.parent().attr('type').value().replace(/-/g, '');
-        localeData.monthNames[context] = localeData.monthNames[context] || {};
-        localeData.monthNames[context][width] = localeData.monthNames[context][width] || {};
-        var monthNo = parseInt(monthNode.attr('type').value(), 10) - 1;
-        localeData.monthNames[context][width][monthNo] = localeData.monthNames[context][width][monthNo] || monthNode.text();
-    });
-
-    localeData.dayNames = {};
-    var dayNoByCldrId = {sun: 0, mon: 1, tue: 2, wed: 3, thu: 4, fri: 5, sat: 6};
-    document.find("/ldml/dates/calendars/calendar[@type='gregorian']/days/dayContext/dayWidth/day").forEach(function (dayNode) {
-        var width = dayNode.parent().attr('type').value().replace(/-/g, ''),
-            context = dayNode.parent().parent().attr('type').value().replace(/-/g, ''),
-            dayNo = dayNoByCldrId[dayNode.attr('type').value()];
-        localeData.dayNames[context] = localeData.dayNames[context] || {};
-        localeData.dayNames[context][width] = localeData.dayNames[context][width] || {};
-        localeData.dayNames[context][width][dayNo] = localeData.dayNames[context][width][dayNo] || dayNode.text();
-    });
-
-    localeData.fields = {};
-    document.find("/ldml/dates/calendars/calendar[@type='gregorian']/fields/field/displayName").forEach(function (fieldDisplayNameNode) {
-        var fieldName = fieldDisplayNameNode.parent().attr('type').value();
-        localeData.fields[fieldName] = localeData.fields[fieldName] || {};
-        localeData.fields[fieldName].displayName = localeData.fields[fieldName].displayName || fieldDisplayNameNode.text();
-    });
-
-    document.find("/ldml/dates/calendars/calendar[@type='gregorian']/fields/field/relative").forEach(function (fieldRelativeNode) {
-        var fieldName = fieldRelativeNode.parent().attr('type').value(),
-            type = fieldRelativeNode.attr('type').value();
-        localeData.fields[fieldName] = localeData.fields[fieldName] || {};
-        localeData.fields[fieldName].relative = localeData.fields[fieldName].relative || {};
-        localeData.fields[fieldName].relative[type] = localeData.fields[fieldName].relative[type] || fieldRelativeNode.text();
-
-    });
-
-    ['date', 'time'].forEach(function (dateOrTime) {
-        var dateOrTimeCapitalized = dateOrTime.replace(/^(\w)/, function (ch) {return ch.toUpperCase();});
-
-        localeData.dateFormats = localeData.dateFormats || {};
-        localeData.dateFormats.basic = localeData.dateFormats.basic || {};
-        document.find("/ldml/dates/calendars/calendar[@type='gregorian']/" + dateOrTime + "Formats/" + dateOrTime + "FormatLength/" + dateOrTime + "Format/pattern").forEach(function (formatNode) {
-            var type = formatNode.parent().parent().attr('type').value();
-            localeData.dateFormats.basic[type + dateOrTimeCapitalized] = localeData.dateFormats.basic[type + dateOrTimeCapitalized] || formatNode.text();
-        });
-    });
-
-    localeData.dateTimePatterns = {};
-    document.find("/ldml/dates/calendars/calendar[@type='gregorian']/dateTimeFormats/dateTimeFormatLength").forEach(function (dateTimeFormatLengthNode) {
-        var dateTimeFormatType = dateTimeFormatLengthNode.attr('type').value(),
-            patternNodes = dateTimeFormatLengthNode.find("dateTimeFormat/pattern");
-        if (patternNodes.length !== 1) {
-            throw new Error('Expected exactly one dateTimeFormat/pattern in $date_time_format_length_node');
-        }
-        localeData.dateTimePatterns[dateTimeFormatType] = localeData.dateTimePatterns[dateTimeFormatType] || patternNodes[0].text();
-    });
-
-    var dateTimeDefaultLengthNode = document.get("/ldml/dates/calendars/calendar[@type='gregorian']/dateTimeFormats/default");
-    if (dateTimeDefaultLengthNode) {
-        localeData.defaultDateTimePatternName = dateTimeDefaultLengthNode.attr('choice').value();
-    }
-
-    document.find("/ldml/dates/calendars/calendar[@type='gregorian']/dateTimeFormats/availableFormats/dateFormatItem").forEach(function (dateTimeFormatNode) {
-       localeData.dateFormats.cldr = localeData.dateFormats.cldr || {};
-       var id = dateTimeFormatNode.attr('id').value();
-       localeData.dateFormats.cldr[id] = localeData.dateFormats.cldr[id] || dateTimeFormatNode.text();
-    });
-
-    var intervalFormatFallbackNode = document.get("/ldml/dates/calendars/calendar[@type='gregorian']/dateTimeFormats/intervalFormats/intervalFormatFallback");
-    if (intervalFormatFallbackNode) {
-        localeData.dateIntervalFallbackFormat = intervalFormatFallbackNode.text();
-    }
-
-    document.find("/ldml/dates/calendars/calendar[@type='gregorian']/dateTimeFormats/intervalFormats/intervalFormatItem").forEach(function (intervalFormatNode) {
-       localeData.dateIntervalFormats = localeData.dateIntervalFormats || {};
-       var dateIntervalFormat = {};
-       intervalFormatNode.childNodes().forEach(function (greatestDifferenceNode) {
-           var idAttribute = greatestDifferenceNode.attr('id');
-           if (!idAttribute) {
-               // Skip whitespace nodes
-               return;
-           }
-           var id = idAttribute.value();
-           dateIntervalFormat[id] = dateIntervalFormat[id] || greatestDifferenceNode.text();
-       });
-       var id = intervalFormatNode.attr('id').value();
-       localeData.dateIntervalFormats[id] = dateIntervalFormat;
-    });
-
-    // Extract unit patterns:
-
-    localeData.unitPatterns = {};
-    document.find("/ldml/units/unit/unitPattern").forEach(function (unitPatternNode) {
-        var unitId = unitPatternNode.parent().attr('type').value().replace(/-([a-z])/g, function ($0, ch) { // year-future => yearFuture etc.
-            return ch.toUpperCase();
-        });
-        localeData.unitPatterns[unitId] = localeData.unitPatterns[unitId] || {};
-        var count = unitPatternNode.attr('count').value();
-        localeData.unitPatterns[unitId][count] = localeData.unitPatterns[unitId][count] || unitPatternNode.text();
-    });
-
-    localeData.delimiters = {};
-    document.find("/ldml/delimiters/*").forEach(function (delimiterNode) {
-        var type = delimiterNode.name();
-        localeData.delimiters[type] = localeData.delimiters[type] || delimiterNode.text();
-    });
-
-    document.find("/ldml/listPatterns/listPattern/listPatternPart").forEach(function (listPatternPartNode) {
-        localeData.listPatterns = localeData.listPatterns || {};
-        var type = listPatternPartNode.attr('type').value();
-        localeData.listPatterns[type] = localeData.listPatterns[type] || listPatternPartNode.text();
-    });
-
-    localeData.exemplarCharacters = {};
-
-    document.find("/ldml/characters/exemplarCharacters").forEach(function (exemplarCharactersNode) {
-        var typeAttr = exemplarCharactersNode.attr('type'),
-            type = (typeAttr && typeAttr.value()) || 'default';
-        localeData.exemplarCharacters[type] = localeData.exemplarCharacters[type] || exemplarCharactersNode.text().replace(/^\[|\]$/g, '').split(" ");
-    });
-
-    localeData.timeZoneDisplayNames = {};
-
-    document.find("/ldml/dates/timeZoneNames/zone").forEach(function (zoneNode) {
-        var timeZoneId = zoneNode.attr('type').value(),
-            exemplarCityNodes = zoneNode.find("exemplarCity"),
-            tzNameLocale;
-        if (exemplarCityNodes.length > 0) {
-            tzNameLocale = exemplarCityNodes[0].text();
-        } else {
-            var genericDisplayNameNodes = zoneNode.find("long/generic");
-            if (genericDisplayNameNodes.length > 0) {
-                tzNameLocale = genericDisplayNameNodes[0].text();
-            } else {
-                var longDisplayNameNodes = zoneNode.find("long/standard");
-                if (longDisplayNameNodes.length > 0) {
-                    tzNameLocale = longDisplayNameNodes[0].text();
-                }
-            }
-        }
-        if (tzNameLocale) {
-            localeData.timeZoneDisplayNames[timeZoneId] = localeData.timeZoneDisplayNames[timeZoneId] || tzNameLocale;
-        }
-    });
-
-    localeData.territoryDisplayNames = {};
-
-    document.find("/ldml/localeDisplayNames/territories/territory").forEach(function (territoryNode) {
-        var territoryId = territoryNode.attr('type').value();
-        localeData.territoryDisplayNames[territoryId] = localeData.territoryDisplayNames[territoryId] || territoryNode.text();
-    });
-
-    localeData.currencyDisplayNames = {};
-    localeData.currencyDisplayNamesCount = {};
-    document.find("/ldml/numbers/currencies/currency/displayName").forEach(function (currencyDisplayNameNode) {
-        var currencyId = currencyDisplayNameNode.parent().attr('type').value(),
-            countAttribute = currencyDisplayNameNode.attr('count');
-        if (countAttribute) {
-            localeData.currencyDisplayNamesCount[currencyId] = localeData.currencyDisplayNamesCount[currencyId] || {};
-            localeData.currencyDisplayNamesCount[currencyId][countAttribute.value()] = currencyDisplayNameNode.text();
-        } else {
-            localeData.currencyDisplayNames[currencyId] = localeData.currencyDisplayNames[currencyId] || currencyDisplayNameNode.text();
-        }
-    });
-
-    localeData.scriptDisplayNames = {};
-    document.find("/ldml/localeDisplayNames/scripts/script").forEach(function (scriptNode) {
-        var scriptId = scriptNode.attr('type').value();
-        localeData.scriptDisplayNames[scriptId] = localeData.scriptDisplayNames[scriptId] || scriptNode.text();
-    });
-
-    return localeData;
-};
-
-cldr.generateGetQuantityMethodAstsFromXmlString = function (xmlString, localeIds) {
-    var document = libxmljs.parseXmlString(xmlString),
-        getQuantityMethodAstByLocaleId = {};
-    localeIds.forEach(function (localeId) {
-        var matchLocalesXPathExpr =
-            "@locales = '" + localeId + "' or " +
-            "starts-with(@locales, '" + localeId + "') or " +
-            "contains(@locales, ' " + localeId + " ') or " +
-            "(contains(@locales, ' " + localeId + "') and substring-after(@locales, ' " + localeId + "') = '')";
-
-        var statementAsts = [],
-            pluralRulesNodes = document.find("/supplementalData/plurals/pluralRules[" + matchLocalesXPathExpr + "]");
-        if (pluralRulesNodes.length > 0) {
-            pluralRulesNodes[0].find("pluralRule").forEach(function (pluralRuleNode) {
-                statementAsts.push(
-                    [
-                        'if',
-                        cldrPluralRuleToJavaScriptAst(pluralRuleNode.text()),
-                        ['return', ['string', pluralRuleNode.attr('count').value()]]
-                    ]
-                );
-            });
-            statementAsts.push(['return', ['string', 'other']]);
-            var functionAst = ['function', null, ['n'], statementAsts];
-            getQuantityMethodAstByLocaleId[localeId] = new Function("n", uglifyJs.uglify.gen_code(['toplevel', statementAsts]));
-        }
-    });
-    return getQuantityMethodAstByLocaleId;
-};

commit 0c1fd513be4b925553458b8549e874db246d47b3

inter.js: this.dateFormats => this.calendars[calendarId].dateFormats (some bugs introduced way back in e634f93f).

lib/inter.js
@@ -825,7 +825,7 @@
             } else {
                 // The exact format wasn't found.
                 // See if we know a similar format that can be rewritten, explanation here: http://unicode.org/cldr/trac/ticket/2641
-                var bestCandidateFormatId = this.getBestICUFormatId(formatId, this.dateFormats.cldr, calendarId);
+                var bestCandidateFormatId = this.getBestICUFormatId(formatId, this.calendars[calendarId].dateFormats.cldr);
                 if (bestCandidateFormatId) {
                     return (this.calendars[calendarId].dateFormats.cldr[formatId] = this.adaptICUFormat(this.calendars[calendarId].dateFormats.cldr[bestCandidateFormatId], formatId));
                 } else {
@@ -833,7 +833,7 @@
                     var matchFormatId = formatId.match(/^y+M+d+$/);
                     if (matchFormatId) {
                         // For some reason there's no yMd fragment in CLDR, adapt the short date format to the required level of detail:
-                        return (this.calendars[calendarId].dateFormats.cldr[formatId] = this.adaptICUFormat(this.dateFormats.basic.shortDate, formatId));
+                        return (this.calendars[calendarId].dateFormats.cldr[formatId] = this.adaptICUFormat(this.calendars[calendarId].dateFormats.basic.shortDate, formatId));
                     }
 
                     matchFormatId = formatId.match(/^([yMQEd]+)([Hhms]+)$/);
@@ -934,8 +934,9 @@
          * @param {String} formatId The CLDR id of the date or date-time
          * format to search for.
          * @param {Object} sourceObject The object to search for
-         * candidates in, could be set to <tt>this.dateFormats.cldr</tt>
-         * or <tt>this.dateIntervalFormats</tt>.
+         * candidates in, could be set to, eg.
+         * `this.calendars.gregorian.dateFormats.cldr` or
+         * `this.calendars.gregorian.dateIntervalFormats`.
          * @return {String} The CLDR id of the best matching format, or
          * undefined if no candidate is found.
          * @private